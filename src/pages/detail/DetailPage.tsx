699afa85485940a3a9f65062744181a65706cccc
ViktorVan22 <zcg5368@163.com>
2022-07-03 08:24:15 +0800

    完成DailyRecords组件

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,8 +1,11 @@
-import Record, { RecordType } from "./components/record/Record";
+import React, { FC } from "react";
+import { groupDailyRecords } from "../../services/recordHelper";
+import DailyRecords from "./components/dailyRecords/DailyRecords";
+import { RecordType } from "./components/record/Record";
 
 const mockRecordList = [
   {
-    timeStamp: 1613477254556, // 2021-02-16 20:07:34:556
+    timeStamp: 1613477254556, // 2021-02-16 20:07:34
     type: RecordType.Expenditure,
     name: "餐饮",
     price: 100,
@@ -10,28 +13,28 @@ const mockRecordList = [
     id: 1,
   },
   {
-    timeStamp: 1613477254556,
+    timeStamp: 1612969810000, // 2021-02-10 23:10:10
     type: RecordType.Expenditure,
     name: "购物",
     price: 200,
     id: 2,
   },
   {
-    timeStamp: 1613477254556,
+    timeStamp: 1612969810000, // 2021-02-10 23:10:10
     type: RecordType.Expenditure,
     name: "蔬菜",
     price: 20,
     id: 3,
   },
   {
-    timeStamp: 1613477254556,
+    timeStamp: 1613477254556, // 2021-02-16 20:07:34
     type: RecordType.Expenditure,
     name: "宠物",
     price: 200,
     id: 4,
   },
   {
-    timeStamp: 1613477254556,
+    timeStamp: 1613477254556, // 2021-02-16 20:07:34
     type: RecordType.Income,
     name: "工资",
     price: 10000,
@@ -40,12 +43,13 @@ const mockRecordList = [
   },
 ];
 
-const DetailPage: React.FC = () => {
+const DetailPage: FC = () => {
+  const groupedDailyRecords = groupDailyRecords(mockRecordList);
   return (
     <div className="detail-page">
       <div className="detail-page-content">
-        {mockRecordList.map(item => (
-          <Record key={item.id} {...item} />
+        {groupedDailyRecords.map(daily => (
+          <DailyRecords key={daily.timeStamp} {...daily} />
         ))}
       </div>
     </div>
---
+++ src/pages/detail/components/dailyRecords/DailyRecords.tsx
@@ -0,0 +1,41 @@
+import { DateFormat, formatTimeStamp } from "../../../../services/dateHelper";
+import { GroupedDailyRecords } from "../../../../services/recordHelper";
+import Record, { RecordItem } from "../record/Record";
+import "./DailyRecords.css";
+
+interface DailyRecordsProps extends GroupedDailyRecords {}
+
+// DailyRecord接受一组 records信息，
+// 且这些records日期均在同一天，且是经过排序后的结果
+const DailyRecords: React.FC<DailyRecordsProps> = ({
+  records,
+  summary,
+  timeStamp,
+}) => {
+  return (
+    <div className={"daily-records"}>
+      <div className={"daily-records-summary"}>
+        <div className={"daily-records-date"}>
+          {formatTimeStamp(timeStamp, DateFormat.MONTH_DAYOFWEEK)}
+        </div>
+        {summary.totalExpenditure > 0 && (
+          <div className={"daily-records-detail"}>
+            支出：-{summary.totalExpenditure}
+          </div>
+        )}
+        {summary.totalIncome > 0 && (
+          <div className={"daily-records-detail"}>
+            收入：+{summary.totalIncome}
+          </div>
+        )}
+      </div>
+      <div className={"records"}>
+        {records.map(record => (
+          <Record key={record.timeStamp} {...record} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DailyRecords;
