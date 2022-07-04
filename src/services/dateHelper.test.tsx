69fdecf30a7400d5ef751dff7d0663397e7ea50f
ViktorVan22 <zcg5368@163.com>
2022-07-04 14:55:28 +0800

    单元测试用例是比注释更好的文档

--- src/services/dateHelper.test.tsx
+++ src/services/dateHelper.test.tsx
@@ -1,14 +1,58 @@
+import moment from "moment";
 import { DateFormat, formatTimeStamp } from "./dateHelper";
 
-// describe用于描述一组测试用例
-// 内部可以包含多个describe或者test方法
 describe("formatTimeStamp function test", () => {
-  // test方法用于描述一个具体的测试用例
+  // 测试 DateFormat.YEAR_MONTH_DAY 的各种情况
   test("should return year-month-day when given timestamp and YEAR_MONTH_DAY format", () => {
-    const timestamp = 1615985092598; // 代表 2021-03-17 20:44:52
-    // 使用jest提供的expect方法做断言判断
-    expect(formatTimeStamp(timestamp, DateFormat.YEAR_MONTH_DAY)).toEqual(
+    const timestamp1 = 1615985092598; // 代表 2021-03-17 20:44:52
+    expect(formatTimeStamp(timestamp1, DateFormat.YEAR_MONTH_DAY)).toEqual(
       "2021-03-17"
     );
+
+    // 测试闰年的 2 月
+    const timestamp2 = 1582905600000; // 代表 2020-02-29 00:00:00
+    expect(formatTimeStamp(timestamp2, DateFormat.YEAR_MONTH_DAY)).toEqual(
+      "2020-02-29"
+    );
+
+    // 测试平年的 2 月
+    const timestamp3 = 1551283200000; // 代表 2019-02-28 00:00:00
+    expect(formatTimeStamp(timestamp3, DateFormat.YEAR_MONTH_DAY)).toEqual(
+      "2019-02-28"
+    );
+  });
+
+  // 测试 DateFormat.MONTH_DAYOFWEEK 的各种情况
+  test("should return month day and day of week when given timestamp and MONTH_DAYOFWEEK format", () => {
+    const timestamp1 = 1615985092598; // 代表 2021-03-17 20:44:52
+    expect(formatTimeStamp(timestamp1, DateFormat.MONTH_DAYOFWEEK)).toEqual(
+      "3月17日 星期三"
+    );
+
+    // 测试闰年的 2 月
+    const timestamp2 = 1582905600000; // 代表 2020-02-29 00:00:00
+    expect(formatTimeStamp(timestamp2, DateFormat.MONTH_DAYOFWEEK)).toEqual(
+      "2月29日 星期六"
+    );
+
+    // 测试平年的 2 月
+    const timestamp3 = 1551283200000; // 代表 2019-02-28 00:00:00
+    expect(formatTimeStamp(timestamp3, DateFormat.MONTH_DAYOFWEEK)).toEqual(
+      "2月28日 星期四"
+    );
+  });
+
+  // 测试 DateFormat.Day 的各种情况
+  test("should return day of month when given timestamp and Day format", () => {
+    const timestamp1 = 1615985092598; // 代表 2021-03-17 20:44:52
+    expect(formatTimeStamp(timestamp1, DateFormat.Day)).toEqual("17");
+
+    // 测试闰年的 2 月
+    const timestamp2 = 1582905600000; // 代表 2020-02-29 00:00:00
+    expect(formatTimeStamp(timestamp2, DateFormat.Day)).toEqual("29");
+
+    // 测试平年的 2 月
+    const timestamp3 = 1551283200000; // 代表 2019-02-28 00:00:00
+    expect(formatTimeStamp(timestamp3, DateFormat.Day)).toEqual("28");
   });
 });
