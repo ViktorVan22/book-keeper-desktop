837bad65107f4735ec18bc2a63b2ac5e0f1d3eae
ViktorVan22 <zcg5368@163.com>
2022-07-04 12:10:59 +0800

    安装recharts并制作demo

--- src/pages/chart/ChartPage.tsx
+++ src/pages/chart/ChartPage.tsx
@@ -1,5 +1,29 @@
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+
 const ChartPage: React.FC = () => {
-  return <div>chart page</div>;
+  // 定义数据
+  const data = [
+    { name: "a", uv: 200 },
+    { name: "b", uv: 100 },
+    { name: "c", uv: 300 },
+    { name: "d", uv: 240 },
+  ];
+
+  return (
+    <div>
+      <LineChart
+        width={600}
+        height={300}
+        data={data}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
+    </div>
+  );
 };
 
 export default ChartPage;
