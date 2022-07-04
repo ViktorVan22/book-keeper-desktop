e262f0c15c7c3cceff4ec4ebf7fd05bcbaebf6df
ViktorVan22 <zcg5368@163.com>
2022-07-04 15:40:42 +0800

    补充recordHelper测试样例

---
+++ src/services/iconSelector.test.tsx
@@ -0,0 +1,44 @@
+import { RecordType } from "../pages/detail/components/record/Record"
+import { getIconByName } from "./iconSelector"
+
+describe('getIconByName function test', () => {
+    test('should return icon icon when give expenditure type and name', () => {
+        expect(getIconByName(RecordType.Expenditure, '餐饮')).toEqual({
+            name: '餐饮',
+            icon: 'icon-food'
+        })
+
+        expect(getIconByName(RecordType.Expenditure, '购物')).toEqual({
+            name: '购物',
+            icon: 'icon-gouwu',
+        })
+
+        expect(getIconByName(RecordType.Expenditure, '日用')).toEqual({
+            name: '日用',
+            icon: 'icon-riyongpin',
+        })
+    })
+
+    test('should return icon icon when we give income type and name', () => {
+        expect(getIconByName(RecordType.Income, '工资')).toEqual({
+            name: '工资',
+            icon: 'icon-salary',
+        })
+
+        expect(getIconByName(RecordType.Income, '兼职')).toEqual({
+            name: '兼职',
+            icon: 'icon-part-time-job',
+        })
+
+        expect(getIconByName(RecordType.Income, '理财')).toEqual({
+            name: '理财',
+            icon: 'icon-financing',
+        })
+    })
+
+    test('should return undefined when give invalid income type and name',
+        () => {
+            expect(getIconByName(RecordType.Expenditure, '工资')).toBeUndefined()
+            expect(getIconByName(RecordType.Income, '日用')).toBeUndefined()
+        })
+})
